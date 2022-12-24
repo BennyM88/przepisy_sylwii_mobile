5aec5b4f5bb37260502e112920264d9873b6386f
Patryk <patryk@kotrem.pl>
2022-12-24 21:57:38 +0100

    Added top bar to home page

--- lib/constants/typography.dart
+++ lib/constants/typography.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:przepisy_sylwii_mobile/constants/colors.dart';
 
 class CustomTypography {
   // Font families:
@@ -8,4 +10,18 @@ class CustomTypography {
   // Font weights:
   static const FontWeight bold = FontWeight.w700;
   static const FontWeight regular = FontWeight.w400;
+
+  static TextStyle uRegular18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: regular,
+    fontFamily: ubuntuFontRegular,
+    color: CustomColors.neutral00,
+  );
+
+  static TextStyle uBold22 = TextStyle(
+    fontSize: 22.sp,
+    fontWeight: bold,
+    fontFamily: ubuntuFontBold,
+    color: CustomColors.neutral00,
+  );
 }
--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:przepisy_sylwii_mobile/constants/typography.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +9,54 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.w),
+          child: Column(
+            children: [
+              SizedBox(height: 20.h),
+              _topBar(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _topBar() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Cześć,',
+              style: CustomTypography.uRegular18,
+            ),
+            const Spacer(),
+            InkWell(
+              onTap: () {},
+              child: Icon(
+                CupertinoIcons.search,
+                size: 24.sp,
+              ),
+            ),
+            SizedBox(width: 28.w),
+            InkWell(
+              onTap: () {},
+              child: Icon(
+                Icons.notes,
+                size: 24.sp,
+              ),
+            ),
+          ],
+        ),
+        Text(
+          'Gal Anonim',
+          style: CustomTypography.uBold22,
+        ),
+      ],
     );
   }
 }
