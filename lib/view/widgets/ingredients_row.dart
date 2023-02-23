62eca962cc08eea45f2b6e61864a6e5090bd4a09
Patryk <patryk@kotrem.pl>
2023-02-23 15:58:56 +0100

    Refactored ui of ingredients row

--- lib/view/widgets/ingredients_row.dart
+++ lib/view/widgets/ingredients_row.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:przepisy_sylwii_mobile/constants/colors.dart';
 import 'package:przepisy_sylwii_mobile/constants/typography.dart';
 import 'package:przepisy_sylwii_mobile/models/recipe.dart';
 
@@ -9,36 +8,24 @@ class IngredientsRow extends StatelessWidget {
   final Recipe recipe;
 
   const IngredientsRow({super.key, required this.index, required this.recipe});
-  //TODO refactor UI
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(bottom: 6.h),
-      child: Container(
-        padding: EdgeInsets.all(8.sp),
-        decoration: BoxDecoration(
-          color: CustomColors.neutral99,
-          borderRadius: BorderRadius.all(Radius.circular(16.r)),
-        ),
-        child: Text(
-          recipe.ingredients[index],
-          style: CustomTypography.uRegular16,
-        ),
+      padding: EdgeInsets.only(bottom: 8.h),
+      child: Row(
+        children: [
+          Text(
+            '⚫️',
+            style: TextStyle(fontSize: 10.sp),
+          ),
+          SizedBox(width: 12.w),
+          Text(
+            recipe.ingredients[index],
+            style: CustomTypography.uRegular16,
+          ),
+        ],
       ),
     );
-    // return Column(
-    //   crossAxisAlignment: CrossAxisAlignment.start,
-    //   children: [
-    //     Row(
-    //       children: [
-    //         Text(
-    //           recipe.ingredients[index],
-    //           style: CustomTypography.uRegular16,
-    //         ),
-    //       ],
-    //     ),
-    //     const Divider(),
-    //   ],
-    // );
   }
 }
